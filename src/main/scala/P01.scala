da34ae6c80f4c191c34abfada469080df102a7bd
liyuntao <liyuntao58607@gmail.com>
2015-05-13 18:50:39 +0800

    P01&P02

--- src/main/scala/P01.scala
+++ src/main/scala/P01.scala
@@ -1,7 +1,19 @@
 package org.p99.scala
 
+/**
+ * P01 (*) Find the last element of a list.
+ * Example:
+ * scala> last(List(1, 1, 2, 3, 5, 8))
+ * res0: Int = 8
+ */
 object P01 {
 
-  // TODO
+  def last[T](list: List[T]): T = {
+    list match {
+      case Nil => throw new NoSuchElementException("no last of Nil")
+      case List(ele) => ele
+      case h :: t => last(t)
+    }
+  }
 
 }
--- src/main/scala/P02.scala
+++ src/main/scala/P02.scala
@@ -1,7 +1,20 @@
 package org.p99.scala
 
+/**
+ * P02 (*) Find the last but one element of a list.
+ * Example:
+ * scala> penultimate(List(1, 1, 2, 3, 5, 8))
+ * res0: Int = 5
+ */
 object P02 {
 
-  // TODO
+  def penultimate[T](lst: List[T]): T = {
+    lst match {
+      case Nil => throw new NoSuchElementException("List is empty")
+      case List(a) => throw new IllegalArgumentException("List has only one element")
+      case x :: List(a) => x
+      case x :: tail => penultimate(tail)
+    }
+  }
 
 }
